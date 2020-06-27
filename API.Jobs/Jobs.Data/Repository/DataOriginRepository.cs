ef6a5c75da798a51f2e03acaab2bf3ff93e9f644
OMEGA-NET <65746648+OMEGA-NET@users.noreply.github.com>
2020-06-27 12:02:00 -0500

    comm

---
+++ API.Jobs/Jobs.Data/Repository/DataOriginRepository.cs
@@ -0,0 +1,38 @@
+﻿using Jobs.Data.Contracts;
+using Jobs.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Jobs.Data.Repository
+{
+    public class DataOriginRepository : IDataOriginRepository
+    {
+        public async Task<List<Product>> ReadProductsFromJson()
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+
+            var jsonString = await File.ReadAllTextAsync("../Jobs.Data/DataOrigin/dbJobs.json");
+            var jsonModel = JsonSerializer.Deserialize<List<Product>>(jsonString, options);
+
+            return jsonModel;
+        }
+
+        public Task<List<Product>> ReadProductsFromSqlLite()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Product>> ReadProductsFromText()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ API.Jobs/Jobs.Models/Emun/TipoProducto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jobs.Models.Emun
+{
+    public enum TipoProducto
+    {
+        placas =1,
+        procesadores =2,
+        otros =3
+    }
+}
